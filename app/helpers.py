383f1c59b378b59d9e88487401d84ca31ca441b5
piccoloser <hunter.gray.logan@gmail.com>
2022-03-14 10:33:36 -0400

    Add Selector class

--- app/helpers.py
+++ app/helpers.py
@@ -1,9 +1,12 @@
 """Generic classes and functions."""
 
 import tkinter as tk
+from typing import Any, Callable
 
 
 class LabeledEntry(tk.Frame):
+    """Simple line-entry with label"""
+
     def __init__(self, root: tk.Frame, text: str):
         super().__init__(root)
 
@@ -12,3 +15,49 @@ class LabeledEntry(tk.Frame):
 
         self.label.grid(row=0, column=0)
         self.entry.grid(row=0, column=1)
+
+
+class Selector(tk.Frame):
+    """Simple dropdown selector"""
+
+    def __init__(
+        self,
+        root: tk.Frame,
+        text: str,
+        options: list[Any] = None,
+        str_func: Callable = None,
+    ):
+        super().__init__(root)
+
+        self._value = tk.StringVar()
+
+        self.label = tk.Label(self, text=text)
+        self.options = SelectorOptions(*options, str_func=str_func)
+        self.menu = tk.OptionMenu(self, self._value, *self.options.list())
+
+        self.label.grid(row=0, column=0)
+        self.menu.grid(row=0, column=1)
+
+    @property
+    def value(self) -> str:
+        return self._value.get()
+
+
+class SelectorOptions:
+    """List of options for a Selector object"""
+
+    def __init__(self, *options, str_func: Callable = None):
+        self._is_liststr = all([isinstance(i, str) for i in options])
+        self._str_func = str_func
+
+        self.items = [*options]
+
+    def list(self) -> list[str]:
+        """Return list of items as strings."""
+        if self._is_liststr:
+            return self.items
+
+        if self._str_func is None:
+            raise TypeError("A function is required to convert non-string options.")
+
+        return [self._str_func(i) for i in self.items]
--- app/templates/default.py
+++ app/templates/default.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
 from .. import template
-from ..helpers import LabeledEntry
+from ..helpers import LabeledEntry, Selector
 
 
 class Template(template.TemplateBase):
+    """Ffiller Template Selector"""
+
     title = "Ffiller (Select a Template)"
     width = 500
     height = 500
@@ -18,8 +20,16 @@ class Template(template.TemplateBase):
         entry1 = LabeledEntry(self.frame, "MyEntry")
         entry1.grid()
 
+        sel1 = Selector(
+            self.frame, "Select an Option", [*range(1, 21)], lambda x: str(x)
+        )
+        sel1.grid()
+
         tk.Button(
             self.frame, text="Test Entry", command=lambda: print(entry1.entry.get())
+        ).grid()
+        tk.Button(
+            self.frame, text="Test Selector", command=lambda: print(sel1.value)
         ).grid()
 
         self.frame.grid()
