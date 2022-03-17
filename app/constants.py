a4354234516a181dc240de8de3b53e58f75086e1
piccoloser <hunter.gray.logan@gmail.com>
2022-03-17 10:54:50 -0400

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -5,6 +5,8 @@ APP_VERSION: str = "v0.0.1"
 GITHUB_RELEASES_URL: str = "https://github.com/piccoloser/ffiller/releases"
 GITHUB_API_URL: str = "https://api.github.com/repos/piccoloser/ffiller/releases"
 
+OUTPUT_PATH: pathlib.Path = pathlib.Path("./output/")
+RESOURCES_PATH: pathlib.Path = pathlib.Path("./app/resources/")
 SETTINGS_PATH: pathlib.Path = pathlib.Path("settings.ini")
 TEMPLATE_PATH: str = "./app/templates/"
 
